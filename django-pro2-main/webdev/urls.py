0aa5de2c877b327f218a673f17592d15b7b3be55
Victor <victorantonio.vats1@gmail.com>
2025-08-14 16:32:41 -0300

    x

---
+++ django-pro2-main/webdev/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib import admin
+from mycontacts import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.show),
+    path('add/', views.add),
+]
+
